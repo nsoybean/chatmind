431d8052dbf0289a35d6fb10d3d780d409f7e8b7
nyangshawbin <shawbin.nyang@circles.asia>
2023-03-15 01:59:25 +0800

    style: prompt question mark style: skip config

--- src/pages/NewChat.jsx
+++ src/pages/NewChat.jsx
@@ -1,4 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Popup from 'reactjs-popup'
+
+import {
+  BsPersonFillAdd,
+  BsChatQuoteFill,
+  BsFillGearFill,
+  BsQuestionCircle
+} from 'react-icons/bs'
+
+function Card({ icon, backgroundColor, title, onClick }) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        padding: '16px',
+        backgroundColor: '#f8f8f8',
+        borderRadius: '8px',
+        boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
+        cursor: 'pointer',
+        width: '100%',
+        marginBottom: '16px'
+      }}
+      onClick={onClick}
+      onMouseOver={(e) => {
+        e.currentTarget.style.transform = 'scale(1.02)'
+      }}
+      onMouseOut={(e) => {
+        e.currentTarget.style.transform = 'scale(1)'
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '48px',
+          height: '48px',
+          backgroundColor: backgroundColor,
+          borderRadius: '8px',
+          marginRight: '16px'
+        }}
+      >
+        {icon}
+      </div>
+      <h3>
+        {title}
+        <BsQuestionCircle
+          size={18}
+          color='#4d4d4d'
+          style={{
+            marginLeft: '8px',
+            cursor: 'pointer'
+          }}
+        />
+      </h3>
+    </div>
+  )
+}
 
 function NewChat() {
   return (
@@ -9,24 +68,50 @@ function NewChat() {
         justifyContent: 'center',
         alignItems: 'center',
         height: '100vh',
-        backgroundColor: '#f8f7fe'
+        backgroundColor: '#f8f7fe',
+        textAlign: 'left',
+        margin: '0 auto' // makes content at the center of page horizontally
       }}
     >
-      <h1 style={{ fontSize: '3rem', marginBottom: '2rem' }}>
-        Welcome to my website
+      <h1 style={{ fontSize: '3rem', marginBottom: '1rem' }}>
+        Great, let's create a chat!
       </h1>
-      <p style={{ fontSize: '1.5rem', maxWidth: '50%', textAlign: 'center' }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus
-        lacus vitae ex pulvinar, sit amet euismod lorem congue. Sed euismod
-        sapien velit, eu rhoncus mauris dapibus ac. Suspendisse potenti. Sed sed
-        arcu quis nulla sodales rutrum. Nam lacinia, quam at commodo imperdiet,
-        ex dolor tempor nulla, eu lacinia ipsum sapien vitae magna. Aenean
-        gravida tempor magna, nec sollicitudin nulla ullamcorper id.{' '}
+      <p style={{ marginTop: '0px', marginBottom: '5rem' }}>
+        Let's begin with the fundamentals. What role would you like ChatGPT to
+        take on?
       </p>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}
+      >
+        <Card
+          icon={<BsPersonFillAdd size={28} color='#0047AB' />}
+          backgroundColor='#e6f2ff'
+          title='Choose an avatar'
+          onClick={() => console.log('Avatar card clicked')}
+        />
+        <Card
+          icon={<BsChatQuoteFill size={28} color='#C04000' />}
+          backgroundColor='#FFDEAD'
+          title='Choose a prompt'
+          onClick={() => console.log('Prompt card clicked')}
+        />
+        <Card
+          icon={<BsFillGearFill size={28} color='#6B3E8C' />}
+          backgroundColor='#E6E6FA'
+          title='Choose system config'
+          onClick={() => console.log('System config card clicked')}
+        />
+      </div>
+      {/* comment out redundant CTA button for now */}
       <button
         style={{
-          padding: '1rem 2rem',
-          backgroundColor: '#4CAF50',
+          padding: '0.8rem 2rem',
+          backgroundColor: '#7393B3',
           color: 'white',
           border: 'none',
           borderRadius: '0.5rem',
@@ -35,7 +120,7 @@ function NewChat() {
           cursor: 'pointer'
         }}
       >
-        Learn More
+        Skip
       </button>
     </div>
   )
