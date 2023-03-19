69cb5946e23ec3fd7631491a4de4793dd9f990d0
nyangshawbin <shawbin.nyang@circles.asia>
2023-03-19 23:45:39 +0800

    feat: user logo beside chat bubble basic implentatino

--- src/components/ChatConversation.jsx
+++ src/components/ChatConversation.jsx
@@ -36,23 +36,43 @@ const ChatConversation = ({ messages }) => {
             display: 'flex',
             // USERS AND CHATGPT CHAT RENDERED ON RIGHT AND LEFT RESPECTIVELY
             justifyContent:
-              message.sender === 'USER' ? 'flex-end' : 'flex-start'
+              message.sender === 'USER' ? 'flex-end' : 'flex-start',
+            alignItems: 'start' // vertically align logo and text
           }}
           onMouseEnter={() => handleMouseEnter(index)}
           onMouseLeave={() => handleMouseLeave()}
         >
+          {message.sender === 'CHATGPT' ? (
+            <img
+              src='chatGPTt-logo.png'
+              alt='chatGPT'
+              style={{ margin: '8px 8px', width: '24px', height: '24px' }}
+            />
+          ) : (
+            ''
+          )}
           <p
             style={{
               backgroundColor: message.sender === 'USER' ? '#3b82f6' : 'white',
               color: message.sender === 'USER' ? 'white' : 'black',
               padding: '10px',
               borderRadius: '15px',
-              width: 'fit-content',
+              // width: 'fit-content',
+              maxWidth: '80%', // chat bubble not taking up full width
               margin: '8px 0px'
             }}
           >
             {message.msg}
           </p>
+          {message.sender === 'USER' ? (
+            <img
+              src='user-logo.png'
+              alt='User'
+              style={{ margin: '8px 8px', width: '24px', height: '24px' }}
+            />
+          ) : (
+            ''
+          )}
           {hoveredChat === index && (
             <div>
               <button onClick={() => handleEdit(index, 'edited message')}>
